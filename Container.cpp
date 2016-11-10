f4bddb9608ca04a82c3489c76a8dab6b5a3eb683
deathsimon <deathsimon@gmail.com>
2016-11-10 11:21:03 +0800

    add methods to container classes

--- Container.cpp
+++ Container.cpp
@@ -1,9 +1,45 @@
+/**********************************
+ * FILE NAME: Container.cpp
+ *
+ * DESCRIPTION:
+ **********************************/
+
 #include "Container.h"
 
-AppContainer::AppContainer()
-{
+
+/**
+ * Constructor of the AppContainer class
+ */
+AppContainer::AppContainer(unsigned int cores, unsigned int memory, unsigned int bandwidth) {
+	/* Set the upper-bounds */
+	bound_Core = cores;
+	bound_Memory = memory;
+	bound_Bandwidth = bandwidth;
+	/* Set the number of users and objects */
+	update(0, 0);
+};
+
+void AppContainer::update(int usrs, int objs) {
+	updateUsr(usrs);
+	updateObj(objs);
+	
+	updateStatus();
+}
+void AppContainer::updateUsr(int usrs) {
+	_usrs = usrs;
+}
+void AppContainer::updateObj(int objs) {
+	_objs = objs;
 }
 
-AppContainer::~AppContainer()
-{
+/**
+ * Constructor of the VRChatroom class
+ */
+void VRChatroom::updateStatus() {
+	// [TODO] update resource requirement according to current _usrs and _objs
+	/*
+	usage_Core = ;
+	usage_Memory = ;
+	usage_Bandwidth = ;
+	*/
 }
--- Container.h
+++ Container.h
@@ -1,18 +1,51 @@
+/**********************************
+* FILE NAME: Container.h
+*
+* DESCRIPTION:
+**********************************/
+
 #include "stdincludes.h"
 
-class AppContainer
-{
+// Marcos
+#define DFLT_CORE	4
+#define DFLT_MMRY	1024
+#define DFLT_BAND	256
+
+/**
+ * CLASS NAME: AppContainer
+ *
+ * DESCRIPTION: the abstract class for container
+ */
+class AppContainer {
 public:
-	AppContainer();
-	~AppContainer();
+	AppContainer() : AppContainer(DFLT_CORE, DFLT_MMRY, DFLT_BAND) {};
+	AppContainer(unsigned int cores, unsigned int memory, unsigned int bandwidth);	
 
+	void update(int usrs, int objs);
+	void updateUsr(int usrs);
+	void updateObj(int objs);
+
+	virtual void updateStatus() = 0;
 private:
-
+	/* Upper-bound of resources */
+	unsigned int bound_Core;
+	unsigned int bound_Memory;
+	unsigned int bound_Bandwidth;
+	/* Actual usage of resources */
+	double usage_Core;
+	unsigned int usage_Memory;
+	double usage_Bandwidth;
+	/* Number of users and objects in the container */
+	unsigned int _usrs;
+	unsigned int _objs;
 };
 
+/**
+ * CLASS NAME: VRChatroom
+ *
+ * DESCRIPTION:
+ */
 class VRChatroom : public AppContainer {
 public:
-	VRChatroom() : AppContainer() {};		
-private:
-
+	void updateStatus();
 };
