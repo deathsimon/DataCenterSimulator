dabfe68aeedb7125860f292212adf535ccce1125
deathsimon <deathsimon@gmail.com>
2016-11-10 15:16:18 +0800

    add methods to server class

--- Server.cpp
+++ Server.cpp
@@ -1,9 +1,52 @@
 #include "Server.h"
 
-Server::Server() {
+/**
+ * Constructor of the Server class
+ */
+Server::Server(unsigned int cores, unsigned int memory, unsigned int bandwidth) {
+	/* Set the upper-bounds */
+	total_Core = cores;
+	total_Memory = memory;
+	total_Bandwidth = bandwidth;
+
 	containers.clear();
 }
+/**
+ * Destructor
+ */
 Server::~Server() {
 	containers.clear();
+
+	printf("Server shutdown\n");
+}
+
+void Server::getCurrUsage(double *uCore, unsigned int *uMemory, double *uBandwidth){
+	(*uCore) = usage_Core;
+	(*uMemory) = usage_Memory;
+	(*uBandwidth) = usage_Bandwidth;
+}
+
+double Server::getScore(AppContainer * targetContainer){
+	// [TODO] return the estimated score for accepting target container
+	// -1.0 if cannot accomdate target container
+	return 0.0;
+}
+
+void Server::deployContainer(AppContainer * targetContainer){
+	containers.push_back(targetContainer);
+
+	distributeResource();
+}
+
+void Server::update(){
+	distributeResource();
+}
+
+void Server::distributeResource(){
+	// [TODO] distribute resources to the hosting containers
+	// update the current resource usage
+	for each (AppContainer* c in containers)	{
+
+	}
 }
 
--- Server.h
+++ Server.h
@@ -1,10 +1,45 @@
+/**********************************
+ * FILE NAME: Server.h
+ *
+ * DESCRIPTION:
+ **********************************/
+
 #include "stdincludes.h"
 #include "Container.h"
 
+// Marcos
+#define SERVER_CORE	16
+#define SERVER_MMRY	10240
+#define SERVER_BAND	1024
+
+/**
+ * CLASS NAME: Server
+ *
+ * DESCRIPTION:
+ */
 class Server {
-public:
-	Server();
-	~Server();
+public:	
+	Server() : Server(SERVER_CORE, SERVER_MMRY, SERVER_BAND) {};
+	Server(unsigned int cores, unsigned int memory, unsigned int bandwidth);	
+	~Server();	
+	/* fetch server information */
+	void getCurrUsage(double *uCore, unsigned int *uMemory, double *uBandwidth);
+	double getScore(AppContainer* targetContainer);
+	/* assign target container to the server */
+	void deployContainer(AppContainer* targetContainer);
+	/* update the resource usage of the server*/
+	void update();
+	
 private:
-	vector<AppContainer*> containers;
+	void distributeResource();
+	/* hardeare resources */
+	unsigned int total_Core;
+	unsigned int total_Memory;
+	unsigned int total_Bandwidth;
+	/* Actual usage of resources */
+	double usage_Core;
+	unsigned int usage_Memory;
+	double usage_Bandwidth;
+	/* hosting containers */
+	vector<AppContainer*> containers;	
 };
